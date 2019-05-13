146a0f66a789f64b75e962477f66cf753291d170
amikchowdhury <amikc1997@gmail.com>
2019-05-13 14:59:40 -0400

    Adding function that allows for movies to be searched by a single attribute.

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
+# create function that asks users how they want to look up a movie
+def search_movie(attribute):
+    attribute = input('What would you like to look up the movie by? ')
+    if attribute == 'name':
+    user_movie_name = input('What is the name of the movie you want to look up? ')
+        for movies in movies_list:
+            for name in movies:
 
-movies = []
+
+
+
+movies_list = []
+
+movie_1 = {
+    'name': 'The Matrix',
+    'director': 'Wachowskis',
+    'year': '1994'
+}
+
+movies_list.append(movie_1)
+print(movies_list)
