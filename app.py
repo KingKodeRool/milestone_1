9082ab3afa935b0a551c535c7d31a07afcacdde2
amikchowdhury <amikc1997@gmail.com>
2019-05-17 16:20:26 -0400

    *Corrected the infinite loop by adding menu prompt to the end of while loop. *Built the add() function.  - It now works with the menu() function call.

--- app.py
+++ app.py
@@ -7,31 +7,77 @@ This needs to have a text base user interface that asks the user what they want 
 
 Things that need to be done:
 
-[x] Think of where to store movies
-[x] How will a movie be formatted
+[] Think of where to store movies
+[] How will a movie be formatted
 [x] Show user the main text interface and get their input
 [] Allow user to add movie(define function)
 [] Allow user to see their movies(define function)
 [] Allow user to find a movie(define function)
-[] Allow user to exit app(define function)
+[x] Allow user to exit app(define function)
 """
 
+movies = []
+'''
+movie format = 
+{
+    'title': ... ,
+    'director': ... ,
+    'year': ... ,
+}
+'''
 
+
+# Defined the menu function. This splits up the different parts of our program. Essentially a hub for other functions.
 def menu():
     user_interface_input = input('Enter one of the following:'
                                  '\n"add"  -  to add a movie to the collection '
                                  '\n"see"  -  to look at the current collection of movies '
                                  '\n"find" -  to loop up a movie'
-                                 '\n"exit" -  to leave out of the application')
+                                 '\n"exit" -  to leave out of the application'
+                                 '\n')
 
-    if user_interface_input == 'add':
-        add_movie()
+    while user_interface_input != 'exit':
+        if user_interface_input == 'add':
+            add_movie()
 
-    elif user_interface_input == 'see':
-        see_movie()
+        elif user_interface_input == 'see':
+            see_movie()
 
-    elif user_interface_input == 'find':
-        find_movie()
+        elif user_interface_input == 'find':
+            find_movie()
 
-    elif user_interface_input == 'exit':
-        print('Exiting the program.')
+        else:
+            print('Invalid input: Please enter a valid option')
+
+        user_interface_input = input('Enter one of the following:'
+                                     '\n"add"  -  to add a movie to the collection '
+                                     '\n"see"  -  to look at the current collection of movies '
+                                     '\n"find" -  to loop up a movie'
+                                     '\n"exit" -  to leave out of the application'
+                                     '\n')
+
+
+def add_movie():
+    movie_title = input('Enter the title of the movie: ')
+    movie_director = input('Enter the director: ')
+    movie_year = int(input('Enter the year the movie was released: '))
+
+    movie_data = dict()
+    movie_data['Title'] = movie_title
+    movie_data['Director'] = movie_director
+    movie_data['Year'] = movie_year
+    print('You have entered this movie: {}'.format(movie_data))
+    movies.append(movie_data)
+    '''
+    Easier way to make a dictionary and append to list:
+    
+    movies.append({
+        'Title': movie_title,
+        'Director': movie_director,
+        'Year': movie_year
+    })
+    '''
+
+
+menu()
+print(movies)
